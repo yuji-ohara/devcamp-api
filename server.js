ebe0aecd42feea9719b36ae2ee0b2377b41f5437
oharayuji <ca.ohara@gmail.com>
2020-09-22 20:01:25 -0300

    rate limit e hpp

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
 
 dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT;
@@ -30,6 +33,9 @@ if(process.env.NODE_ENV === 'development') {
 app.use(fileupload());
 app.use(mongoSanitize());
 app.use(helmet());
+app.use(xss());
+app.use(rateLimit());
+app.use(hpp());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
